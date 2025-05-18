c287e9093c1ab0a3b8b2cb20231fe077cf5a30a8
Karan S Khedkar <103458258+karanskhedkar@users.noreply.github.com>
2025-05-18 17:26:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+const time = document.getElementById("timer");
+document.addEventListener("DOMContentLoaded", function () {
+  setInterval(() => {
+    const currentDate = new Date();
+    const date = currentDate.getDate();
+    const month = currentDate.getMonth();
+    const fullYear = currentDate.getFullYear();
+    const hours = currentDate.getHours();
+    const minutes = currentDate.getMinutes();
+    const seconds = currentDate.getSeconds();
+    const newTime=`${month}/${date}/${fullYear}, ${hours<12?hours:hours-12}:${minutes}:${seconds} ${hours<12?"AM":"PM"}`
+    time.textContent=newTime;
+  }, 1000);
+});
